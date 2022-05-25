b4230e54b0d2152c4cdb1b240a2c7cd29c521aff
CRH <18218292802@163.com>
2022-05-25 19:25:46 +0800

    开放APP首页、商品详情部分接口的游客权限；添加修改商品名称为不能重复

--- mail-admin/src/main/java/com/ys/mail/service/impl/PmsProductServiceImpl.java
+++ mail-admin/src/main/java/com/ys/mail/service/impl/PmsProductServiceImpl.java
@@ -2,17 +2,28 @@ package com.ys.mail.service.impl;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.ys.mail.constant.NumberConstant;
+import com.ys.mail.constant.StringConstant;
 import com.ys.mail.entity.PmsProduct;
+import com.ys.mail.exception.ApiAssert;
+import com.ys.mail.exception.code.CommonResultCode;
 import com.ys.mail.mapper.PmsProductMapper;
 import com.ys.mail.model.admin.query.PmsProductQuery;
 import com.ys.mail.model.dto.ProductInfoDTO;
 import com.ys.mail.model.vo.PmsProductVO;
 import com.ys.mail.service.PmsProductService;
+import com.ys.mail.util.BlankUtil;
+import com.ys.mail.util.IdWorker;
+import com.ys.mail.wrapper.SqlLambdaQueryWrapper;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
 
 
 /**
@@ -39,6 +50,43 @@ public class PmsProductServiceImpl extends ServiceImpl<PmsProductMapper, PmsProd
     }
 
     @Override
+    public boolean addOrUpdate(PmsProduct pmsProduct) {
+        Long id = pmsProduct.getProductId();
+        ApiAssert.noValue(id, CommonResultCode.NULL_USER_ID);
+
+        PmsProduct dbProduct = new PmsProduct();
+        BeanUtils.copyProperties(pmsProduct, dbProduct);
+        String productName = pmsProduct.getProductName();
+        boolean existsName;
+        if (id.equals(NumberUtils.LONG_ZERO)) {
+            // 添加
+            dbProduct.setProductId(IdWorker.generateId());
+            // 名称重复检测
+            existsName = this.isExistsProductName(productName);
+            ApiAssert.isTrue(existsName, CommonResultCode.ERR_PARAM_EXIST.getMessage(productName));
+        } else {
+            // 修改
+            dbProduct = this.getById(id);
+            ApiAssert.noValue(dbProduct, CommonResultCode.ID_NO_EXIST);
+
+            // 名称重复检测
+            if (!dbProduct.getProductName().equals(productName)) {
+                existsName = this.isExistsProductName(productName);
+                ApiAssert.isTrue(existsName, CommonResultCode.ERR_PARAM_EXIST.getMessage(productName));
+            }
+
+            // 恢复会员价
+            Integer promotionType = pmsProduct.getPromotionType();
+            if (BlankUtil.isNotEmpty(promotionType) && !promotionType.equals(NumberConstant.TWO)) {
+                dbProduct.setMebPrice(NumberUtils.LONG_ZERO);
+                dbProduct.setDisCount(new BigDecimal(1));
+            }
+        }
+
+        return this.saveOrUpdate(dbProduct);
+    }
+
+    @Override
     public ProductInfoDTO getProductInfo(Long productId) {
         return pmsProductMapper.selectProductInfo(productId);
     }
@@ -47,5 +95,14 @@ public class PmsProductServiceImpl extends ServiceImpl<PmsProductMapper, PmsProd
     public boolean delete(Long productId) {
         // 商品删除的同时必须连着sku一起删除,否则置换的时候会出现问题
         return pmsProductMapper.delById(productId);
+    }
+
+    @Override
+    public boolean isExistsProductName(String productName) {
+        SqlLambdaQueryWrapper<PmsProduct> wrapper = new SqlLambdaQueryWrapper<>();
+        wrapper.eq(PmsProduct::getProductName, productName)
+               .last(StringConstant.LIMIT_ONE);
+        PmsProduct pmsProduct = this.getOne(wrapper);
+        return BlankUtil.isNotEmpty(pmsProduct);
     }
 }
